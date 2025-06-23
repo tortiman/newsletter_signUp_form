a98ad6561479a14c11aeb7f6b96f1747045a78c4
tortiman <josejavierbedmar@gmail.com>
2025-06-23 18:09:01 +0200

    error state design and javascript regexp pattern

---
+++ js/script.js
@@ -0,0 +1,48 @@
+function emailValidator(email){
+    const regExpExpresion=/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regExpExpresion.test(email);
+}
+
+document.addEventListener('DOMContentLoaded',()=>{
+    console.log('Escribes en el textbox email');
+    emailAddress=document.getElementById('emailId');
+    buttonSubmit=document.getElementById('submitButton');
+    buttonSubmit.addEventListener('click',()=>{
+        
+        const texto=emailAddress.value;
+        if (emailValidator(texto)){
+            console.log('email correcto');
+            document.querySelector('.email-error').style.opacity=0;
+            document.querySelector('.email-error').style.visibility='hidden';
+            document.querySelector('.email-address').style.color='hsl(0, 0%,58%)';
+            document.querySelector('.email-address').style.border='1px solid hsl(0, 0%,58%)';
+            document.querySelector('.email-address').style.backgroundColor='white';
+        }else{
+            document.querySelector('.email-error').style.opacity=1;
+            document.querySelector('.email-error').style.visibility='visible';
+            document.querySelector('.email-address').style.color='hsl(4, 100%, 67%)';
+            document.querySelector('.email-address').style.border='1px solid hsl(4, 100%, 67%)';
+            document.querySelector('.email-address').style.backgroundColor='#FFE8E6';
+        }   
+
+    });
+
+    emailAddress.addEventListener('keydown',(e)=>{
+        const texto=emailAddress.value;
+        if (emailValidator(texto)){
+            console.log('email correcto');
+            document.querySelector('.email-error').style.opacity=0;
+            document.querySelector('.email-error').style.visibility='hidden';
+            document.querySelector('.email-address').style.color='hsl(0, 0%,58%)';
+            document.querySelector('.email-address').style.border='1px solid hsl(0, 0%,58%)';
+            document.querySelector('.email-address').style.backgroundColor='white';
+        }else{
+            document.querySelector('.email-error').style.opacity=1;
+            document.querySelector('.email-error').style.visibility='visible';
+            document.querySelector('.email-address').style.color='hsl(4, 100%, 67%)';
+            document.querySelector('.email-address').style.border='1px solid hsl(4, 100%, 67%)';
+            document.querySelector('.email-address').style.backgroundColor='#FFE8E6';
+        }
+        
+    });
+});
